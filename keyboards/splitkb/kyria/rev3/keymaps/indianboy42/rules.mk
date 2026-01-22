c93a7215eb136e0cad6f0ca1c661d5c59bb01824
Anshuman <amedhi@connect.ust.hk>
2026-01-22 16:08:44 +0800

    kyria rev3

---
+++ keyboards/splitkb/kyria/rev3/keymaps/indianboy42/rules.mk
@@ -0,0 +1,15 @@
+# OLED_ENABLE = yes
+# OLED_DRIVER = ssd1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+CONVERT_TO=promicro_rp2040
+# Keyboard firmware features
+CAPS_WORD_ENABLE = yes
+FORCE_NKRO = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+LEADER_ENABLE = yes
+UNICODE_COMMON = yes
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
